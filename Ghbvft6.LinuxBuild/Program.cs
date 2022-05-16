759f8cddf2f921d7d58c805b3af3a3cf5786846e
greg-chuchro <gchuchro@acm.org>
2022-05-16 21:59:18 +0900

    init

---
+++ Ghbvft6.LinuxBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ghbvft6.LinuxBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
